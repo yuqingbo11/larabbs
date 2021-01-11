8bc0a4c7eadb950ba874c746442d198c60141eea
vagrant <vagrant@homestead>
2021-01-11 14:17:39 +0000

    编辑个人资料

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,4 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 //用户
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
