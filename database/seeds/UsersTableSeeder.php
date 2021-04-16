823aaf6c3ae3662a662f4e3bbd250ca1531cdf0c
vagrant <vagrant@homestead>
2021-04-16 14:05:06 +0000

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -39,10 +39,15 @@ class UsersTableSeeder extends Seeder
 
         // 单独处理第一个用户的数据
         $user = User::find(1);
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
 
     }
 }
